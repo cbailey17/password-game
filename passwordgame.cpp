3310eaf7ff436fa8f562cfc7bdcd546c20f52332
cbailey17 <31027014+cbailey17@users.noreply.github.com>
2020-06-25 17:56:53 -0700

    Update passwordgame.cpp
    fixed and finished tokenDetector

--- passwordgame.cpp
+++ passwordgame.cpp
@@ -10,26 +10,13 @@ using namespace std;
 
 struct tokenDetector {
     map<string, int>  fileWords;
-    string line;
-    string temp_str;
+    string line, temp_str;
     vector<string> strList;
 
-    vector<string> getUnique(ifstream& file) {
+    vector<string> getUnique(ifstream& file) {  // add tokens from the file to a map in order to make sure theyre unique and then add to the vector
         while (getline(file, line)) {
-            for (int i = 0; i <= line.size(); i++) {
-
-                if (ispunct(line[i])) {
-                    line.erase(i, 1);
-                    i -= 1;
-                }
-
-                if (isupper(line[i])) {
-                    line[i] = tolower(line[i]);
-                }
-            }
-
             stringstream ss(line);
-            while (getline(ss, temp_str, ' ')) {
+            while (!ss.eof() && getline(ss, temp_str, ' ')) {
                 if (fileWords.count(temp_str) > 0) {
                     fileWords[temp_str] += 1;
                 }
@@ -39,19 +26,48 @@ struct tokenDetector {
                 }
             }
         }
-        for (unsigned int i = 0; i < strList.size(); ++i) {
-            cout << strList[i] << endl;
+                                   
+        vector<string> finalList = editString(strList);  // After adding tokens to the map and vector, remove caps and trailing punctuation and remove empties
+        for (unsigned int i = 0; i < finalList.size(); i++) {
+            if (finalList[i].empty()) {
+                finalList.erase(finalList.begin() + i);
+            }
+            cout << finalList[i] << endl;
+        }
+        return finalList;
+    }
+
+    vector<string> editString(vector<string> strList) {  // remove capitalization and call remove punctuation
+        for (unsigned int i = 0; i < strList.size(); ++i) {   
+            string & strPtr = strList[i];
+            removePunct(strPtr);
+
+            if (isupper(strList[i][0])) {
+                strList[i][0] = tolower(strList[i][0]);
+            }
         }
         return strList;
     }
 
+    void removePunct(string &str) {          // remove punctuation 
+        if (ispunct(str[str.size() - 1])) {
+            str.erase(str.size() - 1, 1);     
+            if (str.empty()) { return; }
+            removePunct(str);                // recursive call for more than one punctuation at ending
+        }
+        return;
+    }
 } detector;
 
 
-/*
+
 class PassWordGenerator {
 public:
-    PassWordGenerator(vector<string> tokens) {}
+    PassWordGenerator(vector<string> tokens) {
+        cout << "Password generator class constructed" << endl;
+        int numWords = tokens.size();
+    }
+    PassWordGenerator() {}
 
     string getRandomPassword(int numWords) {}
 
@@ -62,19 +78,9 @@ public:
     bool hasNext() {}  // false condition when curser is at length of tokens
 
 private:
-    vector<string> _tokens;
 
 } passwordGenerator;
 
-PassWordGenerator::PassWordGenerator(vector<string> tokens) : _tokens(tokens) {
-    cout << "Password generator class constructed" << endl;
-    int numWords = tokens.size();
-}
-
-string PassWordGenerator::getRandomPassword(int numWords) {
-
-}
-*/
 
 
 
